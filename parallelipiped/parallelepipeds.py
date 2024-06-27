d53874d4cd4818c65675b9f29a52dadfd29ad85d
turgunbek <omurkanov@gmail.com>
2024-06-27 20:00:14 +0300

    Some additions have been done: htm-file with statistics and picture.

--- parallelipiped/parallelepipeds.py
+++ parallelipiped/parallelepipeds.py
@@ -25,7 +25,96 @@ def get_characteristics_of_parallelipiped(dict_abc: dict) -> tuple[float]:
     return result
 
 
+def generate_html(data_dict):
+    html_content = f"""
+    <!doctype html>
+    <html lang="en">
+    <style>
+        table {{
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+            }}
+
+        td, th {{
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+            }}
+
+        tr:nth-child(even) {{
+            background-color: #dddddd;
+            }}
+    </style>
+
+    <body>
+    <h1>Data Summary</h1>
+
+        <h2>Обработали полученные фигуры и подвели статистику</h2>
+
+            <table style="width:25%">
+                <tr>
+                    <th>Parameter</th>
+                    <th>Value</th>
+                </tr>
+
+                <tr>
+                    <td>Average Diagonal</td>
+                    <td>{data_dict['avg_diag']}</td>
+                <tr>
+                <tr>
+                    <td>Average Volume</td>
+                    <td>{data_dict['avg_volume']}</td>
+                <tr>
+                <tr>
+                    <td>Average Surface Area</td>
+                    <td>{data_dict['avg_surface_area']}</td>
+                <tr>
+                <tr>
+                    <td>Average Alpha</td>
+                    <td>{data_dict['avg_alpha']}</td>
+                <tr>
+                <tr>
+                    <td>Average Beta</td>
+                    <td>{data_dict['avg_beta']}</td>
+                <tr>
+                <tr>
+                    <td>Average Gamma</td>
+                    <td>{data_dict['avg_gamma']}</td>
+                <tr>
+                <tr>
+                    <td>Average Radius of Described Sphere</td>
+                    <td>{data_dict['avg_radius_described_sphere']}</td>
+                <tr>
+                <tr>
+                    <td>Average Volume of Described Sphere</td>
+                    <td>{data_dict['avg_volume_described_sphere']}</td>
+                <tr>
+            </table>
+    </body>
+    </html>
+    """
+
+    with open("data_summary.html", "w") as file:
+        file.write(html_content)
+
+
 def main():
+    pict = """
+    MY FIRST SKRIPT
+
+        /------/
+       /      /|
+      /      / |
+     /------/  |
+    |      |  /
+    |      | /
+    |______|/
+
+    I LOVE PYTHON
+    """
+    print(pict)
+
     f = open('parallelepipeds.json')
     data = json.load(f)
 
@@ -45,15 +134,19 @@ def main():
 
         result_dict[k] = params
 
-    n = len(average_values)
+    n = len(result_dict)
     for k, v in average_values.items():
         average_values[k] = v / n
+
+    print(f'Total number of figures = {len(result_dict)}')
 
     with open("characteristics.json", "w") as outfile:
         json.dump(result_dict, outfile, indent=2)
 
     with open("statistics.json", "w") as outfile:
         json.dump(average_values, outfile, indent=2)
+
+    generate_html(average_values)
 
 
 if __name__ == "__main__":
